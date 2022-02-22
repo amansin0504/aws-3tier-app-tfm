7dfe16152dac25ec09b71154aab8a297efd6c6d9
amansin0504 <singh.amandeep@hotmail.co.in>
2022-02-21 22:40:48 -0500

    web server config

--- web-startup.sh
+++ web-startup.sh
@@ -2,7 +2,8 @@ sudo yum install -y epel-release nginx wget unzip lsof ipset
 
 cd /etc/nginx
 sudo mv nginx.conf nginx.conf.backup
-sudo wget https://raw.githubusercontent.com/cisco-security/Cisco-Validated-Designs/master/safe-cloud-iaas-gcp/safe-gcp-infrastructure/nginx.conf
+sudo wget https://raw.githubusercontent.com/amansin0504/aws-3tier-app-tfm/main/nginx.conf
+sudo sed -i -E 's/appnlbaddress_here/${appnlb}/g' ./nginx.conf
 
 sudo systemctl restart nginx
 sudo systemctl enable nginx
